178c3a34951869e6cf3020f7fd2318987b2b6a40
Marian <mcrofts@ucsd.edu>
2017-02-16 11:56:04 -0800

    Finished lab!

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,22 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/"+idNumber, addProject);
+	
 	console.log("User clicked on project " + idNumber);
+
+	/*callback?*/
+
+	
+	
+
+}
+
+function addProject(result) {
+	console.log(result);
+	$("#project"+result.id).find(".details").html(
+		"<img class=\"detailsImage\" src="+result.image+"></img><br/><h3>"+result.title+"</h3><br/>"+result.summary
+	);
 }
 
 /*
@@ -35,4 +50,14 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette/', changeColors);
+}
+
+function changeColors(res) {
+	var colors = res.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
